81e71d0473282b4f4fad45b3df210df526b7b8c5
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-02-02 15:11:55 +0100

    Installation of the CSS Minify Extract Plugin

--- readme.md
+++ readme.md
@@ -4,6 +4,12 @@ Boilerplate for Webpack apps - from Brad Traversy's 'Modern JS From The Beginnin
 
 Frontend environment for building JavaScript applications
 
+Includes the following features...
+
+- Babel Loader
+- CSS and Style Loaders including CSS Minify Extract Plugin
+- HTML Webpack Plugin
+- Webpack Dev Server Plugin
 
 ## Usage
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');   // To import (require()) the node.js build in 'path' module (to use absolute pathnames etc.)
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // To import (require()) the node.js build in 'html-webpack-plugin' module 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // To import (require()) the node.js build in 'html-webpack-plugin' module we've just installed
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -22,7 +23,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/, // To check for every file that ends with .css
-        use: ['style-loader', 'css-loader'],  // To apply the style loader and the css loader we've installed
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],  // To apply the style loader and the css loader we've installed
       },
       {
         test: /\.js$/,  // To check for every file that ends with .js
@@ -42,5 +43,6 @@ module.exports = {
       filename: 'index.html',
       template: './src/index.html',
     }),
+    new MiniCssExtractPlugin()
   ],
 };
